975daeb16e566f41a222c47e4280b06014406253
SpeedPesto <speederyeti@gmail.com>
2026-04-04 18:18:22 +0200

    update

--- main.py
+++ main.py
@@ -26,28 +26,6 @@ setup_stats(bot)
 setup_messagesStats(bot)
 setup_iaController(bot)
 
-os.makedirs("models", exist_ok=True)
-
-urls = [
-    "https://drive.google.com/file/d/1SkNOdgzLNqOQveKCsOGTcEZO0c4mM4nx/view",
-    "https://drive.google.com/file/d/1zV4eieefZvvsfNF3cSBNjxqUqECB40Hx/view"
-]
-
-for url in urls:
-    file_id = url.split("/d/")[1].split("/")[0]
-
-    output_path = f"models/{file_id}.pth"
-
-    gdown.download(
-        f"https://drive.google.com/uc?id={file_id}",
-        output_path,
-        quiet=False
-    )
-
-    print(f"{output_path} téléchargé")
-
-
-
 #----------------------------------------------------------------------------------------------#
 
 @bot.event
